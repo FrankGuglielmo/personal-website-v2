411de9ba3836f1fd642534e79f671e315740771a
Frank Guglielmo <frankieg1610@gmail.com>
2024-03-29 22:16:30 -0700

    Optimized for mobile

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -32,21 +32,24 @@ export default function HeroSection() {
       {/* Header gradient image */}
       <img src={HeaderGradient} alt="Foreground" className="absolute bottom-0 w-full h-1/5 object-cover z-0" /> {/* Adjust height as needed */}
       {/* Input the usflogo.png on top of the header gradient */}
-      <div className="absolute left-40 bottom-5 z-20" style={{width: '30%', opacity:"50%"}}> {/* Adjust width and height as necessary */}
+      <div className="absolute left-40 bottom-5 z-20 mobile-hidden" style={{width: '30%', opacity:"50%"}}> {/* Adjust width and height as necessary */}
                 <img src={USFLogo} alt="Frank" className="w-full h-full object-cover" />
         </div>
-      <div className="absolute right-20 bottom-20 z-20" style={{width: '30%'}}> {/* Adjust width and height as necessary */}
+      <div className="absolute right-20 bottom-20 z-20 mobile-hidden" style={{width: '30%'}}> {/* Adjust width and height as necessary */}
           <img src={ProfilePicture} alt="Frank" className="w-full h-full object-cover" />
         </div>
 
       {/* Content */}
-      <div className="relative z-20 pt-16 pb-16"> {/* Add padding to ensure content does not touch the edges */}
+      <div className="relative z-20 pt-16 pb-16 optimized-mobile"> {/* Add padding to ensure content does not touch the edges */}
         <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
 
           {/* Navigation */}
           <NavBar />
+          <div className="flex items-center justify-center computer-hidden"> {/* Adjust width and height as necessary */}
+              <img src={ProfilePicture} alt="Frank" className="w-full h-full object-cover" />
+            </div>
           {/* Text content */}
-          <div className="lg:w-1/2 pt-20">
+          <div className="lg:w-1/2 pt-20 optimized-mobile-3">
             <h1 id="typing" className="text-7xl text-white font-bold mb-12">{typedText}</h1>
             <p className="mt-12 text-white text-xl mb-12">
               I'm a Software Engineer studying at the University of San Francisco. Welcome to my site! Here you can find some of the projects I've worked on, my work experience, as well as the technologies I'm most familiar with.
@@ -58,7 +61,6 @@ export default function HeroSection() {
               <a href="https://www.linkedin.com/in/fdguglielmo/" target="_blank" className="inline-block bg-transparent border-2 border-white text-white font-semibold px-8 py-3 rounded-lg hover:bg-white hover:text-purple-700 transition duration-300" rel="noreferrer">Connect With Me!</a>
             </div>
           </div>
-          {/* Social media icons */}
         </div>
       </div>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/heroSection.css';
 
 export default function NavBar() {
   return (
     <nav className="absolute top-0 left-0 w-full z-40">
       <div className="container mx-auto flex justify-between items-center p-4">
         <div className="font-bold text-xl text-white">FrankGuglielmo.com</div>
-        <div className="flex items-center">
+        <div className="flex items-center mobile-hidden">
           <a href="#projects" className="text-white px-4 py-2 hover:text-yellow-300 transition-colors font-semibold">Projects</a>
           <span className="text-white mx-2">|</span>
           <a href="#experience" className="text-white px-4 py-2 hover:text-yellow-300 transition-colors font-semibold">Work Experience</a>
