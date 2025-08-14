05602c3e4c31ca859f1294b410c8b62cc1622b19
Frank Guglielmo <frankieg1610@gmail.com>
2025-08-14 12:56:17 -0400

    mobile safe areas

--- next-site/app/layout.tsx
+++ next-site/app/layout.tsx
@@ -8,6 +8,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "FrankGuglielmo.com",
   description: "Software Engineer Portfolio",
+  viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
 };
 
 export default function RootLayout({
